e0ede151062185a2b73658ccbdc088e50b2a4a0e
Lesley Fernandes Moreira <lesleyfernandes@gmail.com>
2019-05-22 12:47:14 +0100

    Adds new Perch theme functions
    Added the perch_page_attributes, perch_pages_title and perch_pages_navigation functions.

--- Perch.php
+++ Perch.php
@@ -6,19 +6,37 @@ class Twig_Perch_Extension extends Twig_Extension
 		return array(
 			'perchRuntime' => new Twig_Function_Method($this, 'runtime'),
 			'perchContent' => new Twig_Function_Method($this, 'content'),
+			'perchPageAttributes' => new Twig_Function_Method($this, 'page_attributes'),
+			'perchPagesTitle' => new Twig_Function_Method($this, 'pages_title'),
+			'perchPagesNavigation' => new Twig_Function_Method($this, 'pages_navigation'),
 		);
-	}
-
-	public function content($region)
-	{
-		return perch_content($region);
 	}
 
 	public function runtime()
 	{
 		return include('/path/to/runtime.php');
 	}
+	
+	public function content($region)
+	{
+		return perch_content($region);
+	}
 
+	public function page_attributes()
+	{
+		return perch_page_attributes();
+	}
+	
+	public function pages_title()
+	{
+		return perch_pages_title();
+	}
+	
+	public function pages_navigation($nav)
+	{
+		return perch_pages_navigation($nav);
+	}
+	
 	public function getName()
 	{
 		return 'perch';
